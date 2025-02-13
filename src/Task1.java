77fb91840b5c27f9a2ee6df4966762bef85bebdf
Zulfiyye <zulfiyyerehimova347@gmail.com>
2025-02-13 20:51:38 +0400

    new task

---
+++ src/Task1.java
@@ -0,0 +1,18 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        int number = 6;
+
+        for(int i=1;i<=10;i++){
+            System.out.printf("%s x %s = %s",number,i,number*i);
+            System.out.println();
+
+        }
+
+
+    }
+
+
+
+}
